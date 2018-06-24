da2c026a8146cadbd34d2a5a1cc316d810c4a335
ArunCap1 <39805450+ArunCap1@users.noreply.github.com>
2018-06-24 16:19:43 -0400

    Changed something

--- SampleApp.java
+++ SampleApp.java
@@ -24,6 +24,6 @@ class SampleApp {
 		System.out.println("SampleApp Chaning samething ") ;
 		System.out.println("SampleApp Chaning samething ") ;   
 		System.out.println("SampleApp Chaning samething ") ;
-		System.out.println("Trying to merge in repository");
+		System.out.println("Trying to change in repository");
 	}
 }
